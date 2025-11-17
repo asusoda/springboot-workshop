5a2bfbb5e4716dae37e323d5791a590a63d70c2b
Aryan Keluskar <aryankeluskar6@gmail.com>
2025-11-16 21:15:46 -0700

    clone template

---
+++ src/main/java/com/example/soda_workshop/SodaWorkshopApplication.java
@@ -0,0 +1,13 @@
+package com.example.soda_workshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SodaWorkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SodaWorkshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/soda_workshop/SodaWorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.soda_workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SodaWorkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
